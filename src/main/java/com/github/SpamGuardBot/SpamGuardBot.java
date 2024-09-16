fbb3f981724792af56c76850cfe4b9027a1d3060
Cthu1hu <ursvnick@gmail.com>
2024-09-16 22:07:19 +0500

    /start

---
+++ src/main/java/com/github/SpamGuardBot/SpamGuardBot.java
@@ -0,0 +1,50 @@
+package com.github.SpamGuardBot;
+import com.github.SpamGuardBot.config.BotConfig;
+import jakarta.validation.constraints.NotNull;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Slf4j
+@Component
+public class SpamGuardBot extends TelegramLongPollingBot{
+    final BotConfig config;
+
+    public SpamGuardBot(BotConfig config) { this.config = config; }
+    @Override
+    public String getBotUsername() { return config.getBotName(); }
+    @Override
+    public String getBotToken() { return config.getToken(); }
+    @Override
+    public void onUpdateReceived(@NotNull Update update)
+    {
+        if( update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String memberName = update.getMessage().getFrom().getFirstName();
+
+            switch(messageText){
+                case "/start":
+                    startBot(chatId, memberName);
+                    break;
+                default: log.info("Unexpected message");
+
+            }
+        }
+    }
+    private void startBot(long chatId, String memberName){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Hello, " + memberName + "! I'm a Telegtam bot.");
+        try{
+            execute(message);
+            log.info("Reply sent");
+        }
+        catch(TelegramApiException e){
+            log.error(e.getMessage());
+        }
+    }
+}
