70141eabe4e2dcb85399f8d63bc198ecae55e8b7
Cthu1hu <ursvnick@gmail.com>
2024-09-16 20:05:57 +0500

    Initial commit

---
+++ src/main/java/com/github/SpamGuardBot/SpamGuardBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.SpamGuardBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpamGuardBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpamGuardBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/SpamGuardBot/SpamGuardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SpamGuardBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpamGuardBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
